f91674ea0895720ceb3fb5a3ef64a838b49d42d6
Ayham7i <ayham1010song@gmail.com>
2024-10-15 13:51:39 +0300

    adding the env and home and navbar

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar/>
+
+      <div className="content">
+
+        <h1>Ayham Al-A</h1>
+      </div>
+\
+    </div>
+  );
+}
+
+export default App;
