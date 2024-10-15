ff1cff9e38e96717eb5b77c6234746c575f29aea
Ayham7i <ayham1010song@gmail.com>
2024-10-16 02:59:24 +0300

    Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     const [blogs , setBlogs] = useState([
         {title:"The Website" , body:"websites are pages that shows info" , author:"Ayham Alakhali",id:1},
         {title:"The Internet" , body:"The internet is a new way to connect world together" , author:"Don Ayham",id:2},
-        {title:"The AI" , body:"Ai will role the world and Ayham will same the world" , author:"Mr.Aura",id:3},
+        {title:"The AI" , body:"Ai will role the world and Ayham will same the world" , author:"Ayham Alakhali",id:3},
     ]);
 
 
@@ -16,6 +16,7 @@ const Home = () => {
         <div className="home">
 
             <BlogList blogs = {blogs} title = "All Blogs"  / >
+            <BlogList blogs = {blogs.filter((blog)=>blog.author === "Ayham Alakhali")} title = "Ayham's Blogs"  / >
      
         </div>
         
